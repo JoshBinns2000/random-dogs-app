4356ccfc773f3f5748c61d7b560b585617c32c92
Joshua Binns <Joshua.Binns@softwire.com>
2023-09-27 17:09:30 +0100

    Working play and pause button

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 
 import ImageRow from './Components/ImageRow';
 import Buses from './Components/Buses';
+import YouTube, { YouTubeProps, YouTubeEvent } from 'react-youtube';
+import { YouTubePlayer } from 'youtube-player/dist/types';
 
 function App() {
   var [imageUrl, setImageUrl] = React.useState("https://images.dog.ceo/breeds/terrier-bedlington/n02093647_524.jpg");
@@ -11,6 +13,9 @@ function App() {
   const cycleInterval = useRef<NodeJS.Timeout>();
 
   var [testBool, setTestBool] = React.useState(true);
+
+  var videoPlayer = useRef<YouTubePlayer>();
+  var [videoPlayerAction, setVideoPlayerAction] = React.useState<'Pause' | 'Play'>('Play');
 
   const baseUrl = "https://dog.ceo/api/breeds/image/random";
   const backupDogImage = "https://images.dog.ceo/breeds/terrier-bedlington/n02093647_524.jpg";
@@ -50,16 +55,60 @@ function App() {
     console.log(testBool);
   }
 
-  return (
+  const youtTubeOpts: YouTubeProps['opts'] = {
+    playerVars: {
+      autoplay: 1,
+    },
+    height: '800px',
+    width: '400px',
+  };
+
+  const youTubeStyle: React.CSSProperties = {
+    'position': 'relative',
+    'border': '.5rem solid grey',
+    'borderRadius': '2rem',
+    'overflow': 'hidden',
+    'lineHeight': '0px',
+    'zIndex': 0,
+    'pointerEvents': 'none',
+  };
+
+  const handlePlayButtonClick = () => {
+    if (videoPlayer.current === undefined) return;
+
+    if (videoPlayerAction === 'Play') {
+      setVideoPlayerAction('Pause');
+      videoPlayer.current.playVideo();
+    }
+    else {
+      setVideoPlayerAction('Play');
+      videoPlayer.current.pauseVideo()
+    }
+};
+
+const onVideoPlayerReady = (event: YouTubeEvent) => {
+  videoPlayer.current = event.target;
+};
+
+return (
+  <div className="large-container">
     <div className="container sliding-background">
       <ImageRow imageUrl={imageUrl}></ImageRow>
       <div className="padded">
         <button onClick={() => setIsCycling(!isCycling)}>MANY DOG</button>
       </div>
       <Buses></Buses>
-      <button onClick={handleTestClick}>{testBool.toString()}</button>
+      {/* <button onClick={handleTestClick}>{testBool.toString()}</button> */}
     </div>
-  );
+
+    <div className="youtube-container">
+      <div className="phone-container">
+        <YouTube videoId="xDHCZau61n4" opts={youtTubeOpts} style={youTubeStyle} onReady={onVideoPlayerReady}></YouTube>
+        <button className="phone-button" onClick={handlePlayButtonClick}>{videoPlayerAction}</button>
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default App;
